c5f9316a010e6104503543bb4d0bef51aeadcefb
Griffin Pope <grriffin@gmail.com>
2019-07-06 18:03:36 -0600

    Implemented sortHeap function

--- sortHeap.h
+++ sortHeap.h
@@ -3,93 +3,110 @@
  *    Week 11, Sort Heap
  *    Brother Helfrich, CS 235
  * Author:
- *    <your name>
+ *    Griffin Pope
  * Summary:
  *    This program will implement the Heap Sort
  ************************************************************************/
 #ifndef SORT_HEAP_H
 #define SORT_HEAP_H
-/*
-//namespace custom {
-template <class T>
-class Heap {
-    private:
-        int num;
-        T *array;
 
-    public:
-        Heap(T *t, int num) : num(num) {
-            array = t;
+//namespace custom
+//{
+template <class T>
+class Heap
+{
+private:
+    int num;
+    T *array;
+    void percolateDown(int index);
+    void swap(int swapA, int swapB);
+    void heapify();
+
+public:
+    Heap(T *t, int size)
+    {
+        array = t;
+        num = size;
+        for (int index = num / 2; index >= 0; index--)
+        {
+            percolateDown(index);
         }
-        void percolateDown(int index);
-        void sort();
-        void swap(T swapA, T swapB);
-        void sortHeap(T array[], int n);
+    }
+    T getMax();
+    void deleteMax();
+    void sort();
 };
-*/
 
 /************************************************
 * HEAP :: PERCOLATE DOWN
 * The item at the passed index may be out of heap
 * order. Take care of that little detail!
 ************************************************/
-/*
 template <class T>
-void Heap <T> :: percolateDown(int index)
+void Heap<T>::percolateDown(int index)
 {
-assert(index >= 1 && index <= num);
 
-// do nothing if we are a leaf
-int indexLeft = index * 2;
-if (indexLeft > num)
-return;
-int indexRight = indexLeft + 1;
+    assert(index >= 0 && index <= num);
 
-// if right is the worst of the two, then go that way
-if (indexRight <= num && // there is an index right
-array[indexRight] > array[indexLeft] && // right is bigger than left
-array[indexRight] > array[index]) // right is bigger than parent
+    // do nothing if we are a leaf
+    int indexLeft = index * 2;
+    if (indexLeft > num)
+        return;
+    int indexRight = indexLeft + 1;
+
+    // if right is the worst of the two, then go that way
+    if (indexRight <= num &&                    // there is an index right
+        array[indexRight] > array[indexLeft] && // right is bigger than left
+        array[indexRight] > array[index])       // right is bigger than parent
+    {
+        swap(index, indexRight);
+        percolateDown(indexRight);
+    }
+    // if the left is the worst of the two, then go that way
+    else if (array[indexLeft] > array[index]) // the left is bigger...
+    {
+        swap(index, indexLeft);
+        percolateDown(indexLeft);
+    }
+    // otherwise do nothing
+}
+
+/**************************************************
+* HEAP :: HEAPIFY
+* Make a heap from the associated array
+*************************************************/
+template <typename T>
+void Heap<T>::heapify()
 {
-swap(index, indexRight);
-percolateDown(indexRight);
 }
-// if the left is the worst of the two, then go that way
-else if (array[indexLeft] > array[index]) // the left is bigger...
-{
-swap(index, indexLeft);
-percolateDown(indexLeft);
-}
-// otherwise do nothing
-}
-*/
+
 /**************************************************
 * HEAP :: SORT
 * From a heap, perform the sort. This will serve
 * to remove all the known items from the heap
 *************************************************/
-/*
-template <class T>
-void Heap <T> :: sort()
-{
-while (num > 1)
-{
-// put the highest element at the end of the list and forget about it!
-swap(1, num--);
 
-// percolate the root
-percolateDown(1);
+template <class T>
+void Heap<T>::sort()
+{
+    while (num > 0)
+    {
+        // put the highest element at the end of the list and forget about it!
+        swap(0, num--);
+
+        // percolate the root
+        percolateDown(0);
+    }
 }
-}
-*/
+
 /* swap two indexes */
-/*
 template <typename T>
-void Heap<T>::swap(T swapA, T swapB) {
-    T temp = swapA;
-    swapA = swapB;
-    swapB = temp;
+void Heap<T>::swap(int swapA, int swapB)
+{
+    T temp = array[swapA];
+    array[swapA] = array[swapB];
+    array[swapB] = temp;
 }
-*/
 
 /*****************************************************
 * SORT HEAP
@@ -98,12 +115,12 @@ void Heap<T>::swap(T swapA, T swapB) {
 template <class T>
 void sortHeap(T array[], int n)
 {
-// put the data into the heap data structure
-//Heap <T> heap(array, n);
+    // put the data into the heap data structure
+    Heap<T> heap(array, n);
 
-// sort it
-//heap.sort();
-//return;
+    // sort it
+    heap.sort();
+    return;
 }
 
 #endif // SORT_HEAP_H
